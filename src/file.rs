5d5b5b69602ee7d545f268bc93c14c313fcb19fe
Thomas Berryhill <oats@oatberry.me>
2018-11-03 13:26:22 -0400

    improve run_file() error reporting

--- src/file.rs
+++ src/file.rs
@@ -6,38 +6,48 @@ use std::io::prelude::*;
 use std::io::{BufReader, BufWriter};
 use std::path::Path;
 
+use crate::errors::ParseError;
 use crate::Interpreter;
 use crate::log;
 
 impl Interpreter {
     /// run each line of a file
     pub fn run_file<P>(&self, path: P) -> Result<(), Error>
-        where P: AsRef<Path> + Debug
+    where
+        P: AsRef<Path> + Debug,
     {
-        log::info(format!("running {:?}...", path));
-
         let file = File::open(path)?;
         let buf = BufReader::new(file);
-        let mut lines = buf.lines();
+        let mut lines = buf.lines().enumerate();
 
-        while let Some(Ok(line)) = lines.next() {
+        while let Some((linenum, Ok(line))) = lines.next() {
             if let Err(err) = self.run(line.as_str()) {
-                log::warn("an error ocurred:");
-                log::warn(line);
-                log::warn(err);
+                match err.downcast::<ParseError>() {
+                    Ok(ParseError::Empty) => continue,
+                    Ok(err) => log::warn(format!(
+                        "parsing error on line {}:\n  {}\n  {}",
+                        linenum + 1,
+                        line,
+                        err
+                    )),
+                    Err(err) => log::warn(format!(
+                        "runtime error on line {}:\n  {}\n  {}",
+                        linenum + 1,
+                        line,
+                        err
+                    )),
+                }
             }
         }
 
-        log::info("run_file: done");
         Ok(())
     }
 
     /// save the values in the Env to a runnable file
     pub fn save_env<P>(&self, path: P) -> Result<(), Error>
-        where P: AsRef<Path> + Debug
+    where
+        P: AsRef<Path> + Debug,
     {
-        log::info(format!("saving current env to {:?}...", path));
-
         let file = File::create(path)?;
         let mut buf = BufWriter::new(file);
         writeln!(&mut buf, "; vim: set ft=scheme:")?;
@@ -47,7 +57,6 @@ impl Interpreter {
             writeln!(&mut buf, "(define {} {})", key, value.serialize())?;
         }
 
-        log::info("save_env: done");
         Ok(())
     }
 }
